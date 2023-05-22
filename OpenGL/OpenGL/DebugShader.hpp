d21e2feabbc0a0237d62efe950365fb25da4c0dc
guomiao <>
2023-05-22 23:57:04 +0800

    三角形移动

---
+++ OpenGL/OpenGL/DebugShader.hpp
@@ -0,0 +1,13 @@
+//
+//  DebugShader.hpp
+//  OpenGL
+//
+//  Created by guomiao on 2023/5/21.
+//
+
+#ifndef DebugShader_hpp
+#define DebugShader_hpp
+
+#include <stdio.h>
+
+#endif /* DebugShader_hpp */
--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -1,75 +1,85 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
- 
 #include <iostream>
+#include "DebugShader.hpp"
+#include <string>
+#include <fstream>
 
-
-//#define numVAOs 1
-//
-//GLuint renderingProgram;
-//GLuint vao[numVAOs];
-//
-//GLuint createShaderProgram()
-//{
-//    const char* vshaderSource =
-//        "#version 410 \n"
-//        "void main(void) \n"
-//        "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
-//
-//    const char* fshaderSource =
-//        "#version 410 \n"
-//        "out vec4 color; \n"
-//        "void main(void) \n"
-//        "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
-//
-//    GLuint vShader = glCreateShader(GL_VERTEX_SHADER); // 创建类型 GL_VERTEX_SHADER 的着色器，返回引用它的序号
-//    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER); // 创建类型 GL_FRAGMENT_SHADER 的着色器，返回引用它的序号
-//
-//    glShaderSource(vShader, 1, &vshaderSource, NULL); // 将GLSL代码从字符串载入空着色器对象中
-//    glShaderSource(fShader, 1, &fshaderSource, NULL);
-//    glCompileShader(vShader); // 编译各着色器
-//    glCompileShader(fShader);
-//
-//    GLuint vfProgram = glCreateProgram(); // 创建程序对象，并储存指向它的整数ID
-//    glAttachShader(vfProgram, vShader); // 将着色器加入程序对象
-//    glAttachShader(vfProgram, fShader);
-//    glLinkProgram(vfProgram); // 让GLSL编译器确保它们的兼容性
-//
-//    return vfProgram;
-//}
+using namespace::std;
+extern void PrintShaderLog(GLuint shader);
+extern void CheckOpenGLError();
+extern void PrintProgram(int Program);
 
 #define numVAOs 1
 
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 
+float x = 0.0f; //三角形X轴位置
+float inc = 0.01f; //移动三角形的偏移量
+
+string ReadShaderSource(const char* filePath)
+{
+    string content;
+    ifstream fileStream(filePath, ios::in);
+
+    string line = "";
+    while (!fileStream.eof()) {
+        getline(fileStream, line);
+        content.append(line + "\n");
+    }
+
+    cout << line << endl;
+    fileStream.close();
+    return content;
+}
+
 GLuint createShaderProgram(){
-    const char* vshaderSource =
-        "#version 410 \n"
-        "void main(void) \n"
-        "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+    
+    GLint vertCompiled;
+    GLint fragCompiled;
+    GLint linked;
+    
+    string vertShaderStr = ReadShaderSource("/Users/guomiao/Documents/OpenGL/OpenGL/OpenGL/vertShader.glsl");
+    string fragShaderStr = ReadShaderSource("/Users/guomiao/Documents/OpenGL/OpenGL/OpenGL/fragShader.glsl");
 
-    const char* fshaderSource =
-        "#version 410 \n"
-        "out vec4 color; \n"
-        "void main(void) \n"
-        "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
-
+    const char* vertShaderSrc = vertShaderStr.c_str();
+    const char* fragShaderSrc = fragShaderStr.c_str();
     
     GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-    glShaderSource(vShader, 1, &vshaderSource, NULL);
-    glShaderSource(fShader, 1, &fshaderSource, NULL);
+    glShaderSource(vShader, 1, &vertShaderSrc, NULL);
     glCompileShader(vShader);
+    CheckOpenGLError();
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+    if(vertCompiled != 1)
+    {
+        cout << "vertex compilation failed" << endl;
+        PrintShaderLog(vShader);
+    }
+    
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fShader, 1, &fragShaderSrc, NULL);
     glCompileShader(fShader);
+    CheckOpenGLError();
+    glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+    if(fragCompiled != 1)
+    {
+        cout << "fragment compilation failed" << endl;
+        PrintShaderLog(fShader);
+    }
 
     GLuint vfProgram = glCreateProgram();
     glAttachShader(vfProgram, vShader);
     glAttachShader(vfProgram, fShader);
     glLinkProgram(vfProgram);
-
-
+    CheckOpenGLError();
+    glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+    if(linked != 1)
+    {
+        cout << "linking faild" << endl;
+        PrintProgram(vfProgram);
+    }
+    
     return vfProgram;
 }
 
@@ -82,47 +92,66 @@ void init(GLFWwindow* window)
 
 void display(GLFWwindow* window, double currentTime)
 {
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
     glUseProgram(renderingProgram);
-    glPointSize(100.0f);
-    glDrawArrays(GL_POINTS, 0, 1);
+    
+    x += inc;
+    if(x > 1.0f)
+    {
+        inc = -0.01f;
+    }
+    if(x < -1.0f)
+    {
+        inc = 0.01f;
+    }
+    
+    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
+    glUniform1f(offsetLoc, x);
+    
+    
+    glPointSize(30.0f);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    glClearColor(1.0, 0.0,0.0,1.0);
+    
+
 }
+
+
 int main()
 {
-
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-    
+
     GLFWwindow* window = glfwCreateWindow(600, 600, "guomiao", NULL, NULL);
-    
+
     glfwMakeContextCurrent(window);
-    
+
     glfwSwapInterval(1);
-  
+
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-   
+
     init(window);
-    
+
     while (!glfwWindowShouldClose(window)) {
         display(window, glfwGetTime());
 
 
         glfwSwapBuffers(window);
         glfwPollEvents();
-        
+
     }
-    
+
     glfwDestroyWindow(window);
     glfwTerminate();
-    
+
     exit(EXIT_SUCCESS);
-//    return 0;
 }
